ec0f8b531cb2a712b819d6eb2e8077d888bd175e
FranciscoMarmolejo <francisco.marmolejo@metricamovil.com>
2020-10-09 13:43:34 -0500

    a lot of changes idk what's going on lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,46 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AlertsComponent } from './components/alerts/alerts.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import { AudioContextModule } from 'angular-audio-context';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {TableModule} from 'primeng/table';
+
+
+import {FormsModule} from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AlertsComponent
+    AlertsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000
+    }),
+    NgxAudioPlayerModule,
+    AudioContextModule.forRoot('balanced'),
+    DragDropModule,
+    NgbModule,
+    TableModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
+
+// <button _ngcontent-c1="" aria-haspopup="true" class="buttonAlarms dropdown-toggle" 
+// ngbdropdowntoggle="" type="button" aria-expanded="true"><img _ngcontent-c1="" alt="" height="60" width="60" 
+// src="https://api.metricamovil.com/MMSecurityWebApp/assets/Alert-icon-hk.png">
+// <span _ngcontent-c1="" class="badge badge-light iconD">4</span></button>
--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import * as Tone from 'tone';
+
+//https://tonejs.github.io/
 
 @Component({
   selector: 'app-alerts',
@@ -7,23 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertsComponent{
 
-  constructor() { }
+  constructor( private toastr: ToastrService) { }
 
   show = false;
   more = false;
+  pop  = false;
   vehicle = {id: 'N/A', tipo: 'N/A', hora: 'N/A', driver: 'N/A', ciudad: 'N/A'};
 
   vehicles: any[] = [ 
-    { id: '1-701', alertA: 1, alertB: 0, alertC: 0},
-    { id: '2-501', alertA: 0, alertB: 1, alertC: 0},
+    { id: '1-701', alertA: 0, alertB: 0, alertC: 0},
+    { id: '2-501', alertA: 0, alertB: 0, alertC: 0},
     { id: '3-231', alertA: 0, alertB: 0, alertC: 0},
     { id: '4-671', alertA: 0, alertB: 0, alertC: 0},
-    { id: '5-781', alertA: 0, alertB: 0, alertC: 2},
+    { id: '5-781', alertA: 0, alertB: 0, alertC: 0},
     { id: '6-351', alertA: 0, alertB: 0, alertC: 0},
-    { id: '3-861', alertA: 0, alertB: 1, alertC: 0},
+    { id: '3-861', alertA: 0, alertB: 0, alertC: 0},
     { id: '4-431', alertA: 0, alertB: 0, alertC: 0},
-    { id: '5-001', alertA: 4, alertB: 0, alertC: 0},
-    { id: '6-701', alertA: 0, alertB: 8, alertC: 0},
+    { id: '5-001', alertA: 0, alertB: 0, alertC: 0},
+    { id: '6-701', alertA: 0, alertB: 0, alertC: 0},
   ];  
 
   alertas: any[] = [ 'Botón de Pánico', 'Jammer', 'Paro de motor'];
@@ -38,14 +43,51 @@ export class AlertsComponent{
     this.more = !this.more;
   }
 
-  randomize(){
+  showToastr(){
 
-    let index = Math.floor( Math.random() * 6);
-    let hora = Math.floor( Math.random() * 24);
-    let minutos = hora + 30;
-    let alerta = Math.floor( Math.random() * 3);
-    this.vehicle = {id: this.vehicles[index].id, tipo: this.alertas[alerta], hora: `${hora}:${minutos}pm`, driver: 'Jorge Perez', ciudad: 'Torreón, Coahuila'};
-
+    // (tipoAlerta === 1) ? this.toastr.error('Tenemos un 33-12', 'Alerta: Botón de Pánico') :
+    //   (tipoAlerta === 2) ? this.toastr.warning('Tenemos un 33-12', 'Alerta: Jammer') :  this.toastr.info('Tenemos un 33-12', 'Alerta: Paro de Motor');
+    this.toastr.error('Tenemos un 33-12', 'Alerta: Botón de Pánico');
   }
 
+  randomize(){
+
+    this.showToastr();
+    // this.beep( Math.floor( Math.random() * 3) );
+    // this.showToastr( Math.floor( Math.random() * 3) );
+  }
+
+  alerts(){
+
+    // let index = Math.floor( Math.random() * 6);
+    // let hora = Math.floor( Math.random() * 24);
+    // let minutos = hora + 30;
+    // let alerta = Math.floor( Math.random() * 3);
+    // this.vehicle = {id: this.vehicles[index].id, tipo: this.alertas[alerta], hora: `${hora}:${minutos}pm`, 
+    // driver: 'Jorge Perez', ciudad: 'Torreón, Coahuila'};
+    // this.beep( Math.floor( Math.random() * 3) );
+    // this.showToastr( Math.floor( Math.random() * 3) );
+  }
+  
+
+  beep( tipoAlerta ){
+    const synth = new Tone.Synth().toDestination();
+    (tipoAlerta == 1) ? synth.triggerAttackRelease('G6', '8n') :
+      (tipoAlerta == 2) ? synth.triggerAttackRelease('B2', '8n') :  synth.triggerAttackRelease('D3', '8n');
+  }
+
+  beep1(){
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease('G6', '4n');
+  }
+
+  beep2(){
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease('F5', '4n');
+  }
+
+  beep3(){
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease('B5', '4n');
+  }
 }
